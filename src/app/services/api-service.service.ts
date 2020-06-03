a7fac0a07ee128655330fe56648087b0393ac04f
alissonjosef <alissonjosef@gmail.com>
2020-06-03 20:38:22 -0300

    mago

---
+++ src/app/services/api-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+
+  caminho = 'http://locauhost/crudangular/';
+
+  constructor(private http:HttpClient) { }
+
+  Api(dados:any, api:string){
+    const httpOptions = {
+      headers:new HttpHeaders({'Content-Type': 'application/json'})
+    };
+      const url = this.caminho + api;
+      return this.http.post(url, JSON.stringify(dados), httpOptions).map(res => res);
+  }
+}
